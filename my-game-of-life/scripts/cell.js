041187fb9aa580dbc8b439754ab011eb61c9e5dd
cmosilva <carlosmocsilva@gmail.com>
2023-11-15 15:30:51 +0000

    adaptation to functional programming

--- my-game-of-life/scripts/cell.js
+++ my-game-of-life/scripts/cell.js
@@ -5,44 +5,38 @@
  * Class expression:  const Name = class {}
  */
 
-class Cell {
-  constructor(
-    // Defines parameters:
-    id,
-    state,
-    row,
-    col
-  ) {
-    // Define properties:
-    this.id = id;
-    this.state = state;
-    this.row = row;
-    this.col = col;
-  }
+const Cell = ({
+  id, state, row, col,
+}) => {
+  // Define properties:
+  const _id = id;
+  let _state = state;
+  const _row = row;
+  const _col = col;
 
-  toggleState() {
-    const el = document.querySelector(`#cell-${this.id}`);
-    const numCellsAlive = document.querySelector("#num-cells-alive");
-    el.innerText === "1" ? this.state = 0 : this.state = 1;
-    el.innerText = this.state;
-    el.classList.toggle("live");
-    el.classList.toggle("dead");
-    numCellsAlive.innerHTML = (Number(numCellsAlive.innerHTML) + (this.state === 1 ? 1 : -1)).toString().padStart(3, "0")
-  }
-
-  getHTMLElement() {
-    const div = document.createElement("div");
-    div.classList.add(`grid-item`);
-    div.setAttribute("id", `cell-${this.id}`)
-    div.classList.add(`${this.state === 1 ? "live" : "dead"}`);
-    div.innerText = this.state;
-    div.addEventListener("click", () => {
-      if (document.querySelector(".timer-box").classList.contains("hide")) {
-        this.toggleState();
-      }
-    });
-    return div;
-  }
-}
+  return {
+    toggleState: () => {
+      const el = document.querySelector(`#cell-${_id}`);
+      const numCellsAlive = document.querySelector('#num-cells-alive');
+      el.innerText === '1' ? _state = 0 : _state = 1;
+      el.innerText = _state;
+      el.classList.toggle('live');
+      el.classList.toggle('dead');
+      numCellsAlive.innerHTML = (Number(numCellsAlive.innerHTML) + (_state === 1 ? 1 : -1)).toString().padStart(3, '0');
+    },
+    getHTMLElement: () => {
+      const div = document.createElement('div');
+      div.classList.add('grid-item');
+      div.setAttribute('id', `cell-${_id}`);
+      div.classList.add(`${_state === 1 ? 'live' : 'dead'}`);
+      div.innerText = _state;
+      return div;
+    },
+    getId: () => _id,
+    getState: () => _state,
+    getRow: () => _row,
+    getCol: () => _col,
+  };
+};
 
 export default Cell;
--- my-game-of-life/scripts/script.js
+++ my-game-of-life/scripts/script.js
@@ -1,29 +1,40 @@
 /**
  * Create a Backpack object, populate some HTML to display its properties.
  */
-import Cell from "./cell.js";
+import Cell from './cell.js';
 
-const btnSimulate = document.querySelector("#btn-simulate");
-const btnSimulateN = document.querySelector("#btn-simulate-n");
-const btnSimulateForever = document.querySelector("#btn-simulate-forever");
-const btnStopSimulation = document.querySelector("#btn-stop-simulation");
-const btnExport = document.querySelector("#btn-export");
-const btnReset = document.querySelector("#btn-reset");
-const grid = document.querySelector(".grid");
-const genInput = document.querySelector("#gen-input");
-const selBoard = document.querySelector("#sel-board");
-const genDelayInp = document.querySelector("#gen-delay");
-const numCellsAlive = document.querySelector("#num-cells-alive");
-const counter = document.querySelector(".counter");
-const inputBox = document.querySelector(".input-box");
-const timerBox = document.querySelector(".timer-box");
-const messages = document.querySelector(".messages");
-let boardChanged;
-let nFactor = 0;
-let intervId;
+const btnSimulate = document.querySelector('#btn-simulate');
+const btnSimulateN = document.querySelector('#btn-simulate-n');
+const btnSimulateForever = document.querySelector('#btn-simulate-forever');
+const btnStopSimulation = document.querySelector('#btn-stop-simulation');
+const btnExport = document.querySelector('#btn-export');
+const btnReset = document.querySelector('#btn-reset');
+const grid = document.querySelector('.grid');
+const genInput = document.querySelector('#gen-input');
+const selBoard = document.querySelector('#sel-board');
+const genDelayInp = document.querySelector('#gen-delay');
+const numCellsAlive = document.querySelector('#num-cells-alive');
+const counter = document.querySelector('.counter');
+const inputBox = document.querySelector('.input-box');
+const timerBox = document.querySelector('.timer-box');
+const messages = document.querySelector('.messages');
 const BOARD_SIZE = 20;
 const cells = [];
 const boardMap = new Map();
+let boardChanged;
+let nFactor = 0;
+let intervId;
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+const createMessage = (message) => {
+  messages.innerText = message;
+  setTimeout(() => {
+    messages.innerText = '';
+  }, 3000);
+};
 
 const createCommonForm = (value = null) => {
   const matrix = [];
@@ -38,56 +49,62 @@ const createCommonForm = (value = null) => {
 };
 
 const updateAliveCells = (num) => {
-  numCellsAlive.innerHTML = (Number(numCellsAlive.innerHTML) + num).toString().padStart(3, "0");
-}
+  numCellsAlive.innerHTML = (Number(numCellsAlive.innerHTML) + num).toString().padStart(3, '0');
+};
 
 const generateBoardWithCommonForm = (arr) => {
-  numCellsAlive.innerHTML = "000";
-  grid.innerHTML = "";
-  let count = 0;
-  for (let i = 0; i < arr.length; i++) {
-    const line = [];
-    for (let j = 0; j < arr[i].length; j++) {
-      const cell = new Cell(count, arr[i][j], i, j);
-      line[j] = cell;
-      grid.appendChild(cell.getHTMLElement());
-      count++;
-      updateAliveCells(cell.state);
-    }
-    cells[i] = line;
-  }
+  numCellsAlive.innerHTML = '000';
+  grid.innerHTML = '';
+  arr.forEach((line, i) => {
+    const row = [];
+    line.forEach((val, j) => {
+      const cell = Cell({
+        id: (i * BOARD_SIZE) + j, state: val, row: i, col: j,
+      });
+      const element = cell.getHTMLElement();
+      element.addEventListener('click', () => {
+        if (timerBox.classList.contains('hide')) {
+          cell.toggleState();
+        }
+      });
+      grid.appendChild(element);
+      updateAliveCells(cell.getState());
+      row[j] = cell;
+    });
+    cells[i] = row;
+  });
 };
 
 boardMap.set(0, createCommonForm(1));
 
 for (let i = 1; i < 6; i++) {
   boardMap.set(i, createCommonForm());
-  const opt = document.createElement("option");
-  opt.setAttribute("value", `${i}`);
-  opt.innerText = "Board-" + i;
+  const opt = document.createElement('option');
+  opt.setAttribute('value', `${i}`);
+  opt.innerText = `Board-${i}`;
   selBoard.appendChild(opt);
 }
 
 generateBoardWithCommonForm(boardMap.get(0));
 
-selBoard.addEventListener("change", (ev) => {
+selBoard.addEventListener('change', (ev) => {
   const board = boardMap.get(Number(ev.target.value));
   generateBoardWithCommonForm(board);
 });
 
 const showCounter = () => {
-  if (!inputBox.classList.contains("hide")) {
-    inputBox.classList.toggle("hide");
+  if (!inputBox.classList.contains('hide')) {
+    inputBox.classList.toggle('hide');
   }
-  if (timerBox.classList.contains("hide")) {
-    timerBox.classList.toggle("hide");
+  if (timerBox.classList.contains('hide')) {
+    timerBox.classList.toggle('hide');
   }
   counter.innerHTML = nFactor;
 };
 
 const hideCounter = () => {
-  inputBox.classList.toggle("hide");
-  timerBox.classList.toggle("hide");
+  inputBox.classList.toggle('hide');
+  timerBox.classList.toggle('hide');
 };
 
 const stopTimer = () => {
@@ -95,29 +112,59 @@ const stopTimer = () => {
   hideCounter();
   intervId = undefined;
   if (!boardChanged) {
-    createMessage("No more changes possible");
+    createMessage('No more changes possible');
     genInput.value = nFactor;
   }
 };
 
+const getNearCellsAlive = (cell) => {
+  const isNearCellAlive = (row, col) => cells[row] && cells[row][col]
+      && cells[row][col].getState() === 1;
+
+  let cellsAlive = 0;
+
+  if (isNearCellAlive(cell.getRow(), cell.getCol() - 1)) { cellsAlive++; }
+  if (isNearCellAlive(cell.getRow(), cell.getCol() + 1)) { cellsAlive++; }
+  if (isNearCellAlive(cell.getRow() - 1, cell.getCol() - 1)) { cellsAlive++; }
+  if (isNearCellAlive(cell.getRow() - 1, cell.getCol())) { cellsAlive++; }
+  if (isNearCellAlive(cell.getRow() - 1, cell.getCol() + 1)) { cellsAlive++; }
+  if (isNearCellAlive(cell.getRow() + 1, cell.getCol() - 1)) { cellsAlive++; }
+  if (isNearCellAlive(cell.getRow() + 1, cell.getCol())) { cellsAlive++; }
+  if (isNearCellAlive(cell.getRow() + 1, cell.getCol() + 1)) { cellsAlive++; }
+
+  return cellsAlive;
+};
+
+const changeCellsState = () => {
+  boardChanged = false;
+  const aliveValues = cells.map((line) => line.map((cell) => getNearCellsAlive(cell)));
+  cells.forEach((line, i) => line.forEach((cell, j) => {
+    if ((cell.getState() === 1 && (aliveValues[i][j] < 2 || aliveValues[i][j] > 3))
+        || (cell.getState() === 0 && aliveValues[i][j] === 3)) {
+      cell.toggleState();
+      boardChanged = true;
+    }
+  }));
+};
+
 const runGeneration = (type) => {
   if (!intervId) {
-    const numGen = genInput.value === "" ? 0 : Number(genInput.value);
-    const delay = genDelayInp.value === "" ? 0 : Number(genDelayInp.value);
+    const numGen = genInput.value === '' ? 0 : Number(genInput.value);
+    const delay = genDelayInp.value === '' ? 0 : Number(genDelayInp.value);
     if (Number(numCellsAlive.innerText) === 0) {
-      createMessage("All cells are dead :(");
+      createMessage('All cells are dead :(');
       return;
     }
     if (type === 2 && numGen <= 0) {
-      createMessage("Gen number must be greater than 0");
+      createMessage('Gen number must be greater than 0');
       return;
     }
     if (type === 2 && delay < 0) {
-      createMessage("Must insert a delay equal or greater than 0");
+      createMessage('Must insert a delay equal or greater than 0');
       return;
     }
     if (type === 3 && delay <= 0) {
-      createMessage("Must insert a delay greater than 0");
+      createMessage('Must insert a delay greater than 0');
       return;
     }
     nFactor = 0;
@@ -130,72 +177,22 @@ const runGeneration = (type) => {
       }
     }, type === 1 ? 0 : delay);
   }
-}
+};
 
-btnSimulate.addEventListener("click", () => runGeneration(1));
-btnSimulateN.addEventListener("click", () => runGeneration(2));
-btnSimulateForever.addEventListener("click", () => runGeneration(3));
+btnSimulate.addEventListener('click', () => runGeneration(1));
+btnSimulateN.addEventListener('click', () => runGeneration(2));
+btnSimulateForever.addEventListener('click', () => runGeneration(3));
 
-btnReset.addEventListener("click", () => {
+btnReset.addEventListener('click', () => {
   generateBoardWithCommonForm(boardMap.get(Number(selBoard.value)));
 });
 
-btnStopSimulation.addEventListener("click", () => {
+btnStopSimulation.addEventListener('click', () => {
   if (intervId) {
     stopTimer();
   }
 });
 
-btnExport.addEventListener("click", () => {
-  alert(JSON.stringify(cells.map(line => line.map(cell => cell.state))))
+btnExport.addEventListener('click', () => {
+  alert(JSON.stringify(cells.map((line) => line.map((cell) => cell.getState()))));
 });
-
-const changeCellsState = () => {
-  boardChanged = false;
-  cells.forEach(line => line.forEach(cell => cell.nearCellsAlive = getNearCellsAlive(cell)));
-  cells.forEach(line => line.forEach(cell => {
-    if ((cell.state === 1 && (cell.nearCellsAlive < 2 || cell.nearCellsAlive > 3)) || (cell.state === 0 && cell.nearCellsAlive === 3)) {
-      cell.toggleState();
-      boardChanged = true;
-    }
-  }));
-};
-
-const getNearCellsAlive = (cell) => {
-
-  const isNearCellAlive = (row, col) => {
-    return cells[row] && cells[row][col] && cells[row][col].state === 1;
-  };
-
-  let cellsAlive = 0;
-
-  if (isNearCellAlive(cell.row, cell.col - 1))
-    cellsAlive++;
-  if (isNearCellAlive(cell.row, cell.col + 1))
-    cellsAlive++;
-  if (isNearCellAlive(cell.row - 1, cell.col - 1))
-    cellsAlive++;
-  if (isNearCellAlive(cell.row - 1, cell.col))
-    cellsAlive++;
-  if (isNearCellAlive(cell.row - 1, cell.col + 1))
-    cellsAlive++;
-  if (isNearCellAlive(cell.row + 1, cell.col - 1))
-    cellsAlive++;
-  if (isNearCellAlive(cell.row + 1, cell.col))
-    cellsAlive++;
-  if (isNearCellAlive(cell.row + 1, cell.col + 1))
-    cellsAlive++;
-
-  return cellsAlive;
-};
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-const createMessage = (message) => {
-  messages.innerHTML = message;
-  setTimeout(() => {
-    messages.innerHTML = "";
-  }, 3000);
-};
